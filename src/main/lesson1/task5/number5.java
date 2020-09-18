377e5b93a1fb70b4a389aca0f9cafa3f1ce39ec7
mgrishchenkova <71334332+mgrishchenkova@users.noreply.github.com>
2020-09-18 19:47:06 +0300

    задач5

--- src/main/lesson1/task5/number5.java
+++ src/main/lesson1/task5/number5.java
@@ -1,4 +1,35 @@
 package main.lesson1.task5;
 
+import main.lesson1.task4.number4;
+
 public class number5 {
+    boolean result;
+    public static void main (String[] args){
+        number5 instance = new number5();
+
+        System.out.println("первый метод " + instance.or(false,false));
+        System.out.println("первый метод " + instance.or(false,true));
+        System.out.println("первый метод " + instance.or(true,false));
+        System.out.println("первый метод " + instance.or(true,true));
+        System.out.println("второй метод " + instance.invert(false,false));
+        System.out.println("второй метод " + instance.invert(false,true));
+        System.out.println("второй метод " + instance.invert(true,false));
+        System.out.println("второй метод " + instance.invert(true,true));}
+
+        boolean or ( boolean a,boolean b)
+        {
+
+        result=!(a | b);
+        return result;
+        }
+
+        boolean invert ( boolean a,boolean b)
+        {
+
+          a=!a;
+          b=!b;
+            result=a & b;
+            return result;
+        }
+
 }
