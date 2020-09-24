eb4d78e351b4164008adc3d2e53a06968e8ad9f7
mgrishchenkova <71334332+mgrishchenkova@users.noreply.github.com>
2020-09-24 19:12:55 +0300

    задача3
    Signed-off-by: mgrishchenkova <71334332+mgrishchenkova@users.noreply.github.com>

--- src/main/lesson2/task3/Phone.java
+++ src/main/lesson2/task3/Phone.java
@@ -12,31 +12,35 @@ public class Phone {
     }
 
     boolean phone1(String number) {
+
         return number.matches("^\\+7\\(\\d{3}\\)\\d{3}-\\d{2}-\\d{2}$");
     }
 
-   // boolean phone2For(String number) {
-       // if (candidate == null || candidate.length() != 16)
-         //   return false;
-
-      //  int[] numericIndexes = {3, 4, 5, 7, 8, 9, 11, 12, 14, 15}; // Индексы в строке, которые принимают любые цифровые значения
+    boolean phone2For(String number) {
+        if (number == null || number.length() != 16)
+            return false;
+        int[] numericIndexes = {3, 4, 5, 7, 8, 9, 11, 12, 14, 15}; // Индексы в строке, которые принимают любые цифровые значения
         String numericPattern = "0123456789";
 
         // Проверяем обязательные элементы паттерны
-       // if (!candidate.startsWith("+7("))
-         //   return false;
-      //  if (candidate.charAt(6) != ')')
-        //   return false;
-       // if (candidate.charAt(10) != '-' || candidate.charAt(13) != '-')
-           // return false;
+        if (!number.startsWith("+7("))
+            return false;
+        if (number.charAt(2) != '(')
+            return false;
+        if (number.charAt(6) != ')')
+            return false;
+        if (number.charAt(10) != '-' & number.charAt(13) != '-')
+            return false;
 
         // Проверяем остальные цифры в номере, которые могут принимать любые значения
-      //  for (int index : numericIndexes) {
-          //  String symbol = String.valueOf(candidate.charAt(index));
-         //   if (!numericPattern.contains(symbol))
-               // return false;
+        for (int index : numericIndexes) {
+            String symbol = String.valueOf(number.charAt(index));
+            if (!numericPattern.contains(symbol))
+                return false;
         }
         // Все проверки пройдены успешно, возвращаем true
-       // return true;
+        return true;
+    }
+}
 
 
