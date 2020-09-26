616ec656087e902ce5a9d06eaa42618f17c81ded
mgrishchenkova <71334332+mgrishchenkova@users.noreply.github.com>
2020-09-26 19:49:05 +0300

    задача1
    Signed-off-by: mgrishchenkova <71334332+mgrishchenkova@users.noreply.github.com>

---
+++ src/main/lesson3/task1/Calculator.java
@@ -0,0 +1,18 @@
+package main.lesson3.task1;
+
+public class Calculator {
+    int Summ (int x, int y){
+        return x+y;
+    }
+
+    int substract( int x, int y){
+        return x-y;
+    }
+
+    int multiply(int x, int y){
+        return x*y;
+    }
+    int divide (int x, int y){
+        return x/y;
+    }
+}
