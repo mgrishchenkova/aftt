c2b96a55abc6ce31f295bcc44558dd7c4044d224
mgrishchenkova <71334332+mgrishchenkova@users.noreply.github.com>
2020-09-26 19:17:23 +0300

    траспонирование
    Signed-off-by: mgrishchenkova <71334332+mgrishchenkova@users.noreply.github.com>

--- src/main/lesson2/task5/Transpose.java
+++ src/main/lesson2/task5/Transpose.java
@@ -8,23 +8,34 @@ public class Transpose {
         // часть 1 - создаем матрицу n на m
         int n = new Random().nextInt(3) + 3;
         int m = new Random().nextInt(3) + 3;
-        int[][] a = new int[n][m];
+        int[][] array1 = new int[n][m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                a[i][j] = new Random().nextInt(9);
+                array1[i][j] = new Random().nextInt(9);
             }
         }
 //часть 2 - транспонирование
-        //int[][] aT = new int[m][n];
-     //   for (int i = 0; i < m; i++) {
-     //       for (int j = 0; j < n; j++) {
-      //          aT[i][j] = a[i][j];
-        //    }
-    //    }
+        int[][] arrayTranspose = new int[m][n];
+      for (int i = 0; i < m; i++) {
+          for (int j = 0; j < n; j++) {
+              arrayTranspose[i][j] = array1[j][i];
+           }
 
+       }
+//вывод первоначальной матрицы
+       System.out.println("Первоначальная матрица");
         for (int i = 0; i < n; i++) {
+            System.out.println();
             for (int j = 0; j < m; j++) {
-                System.out.print(a[i][j]);
+                System.out.print(array1[i][j]);
+            }
+        }
+        System.out.println();
+     System.out.println("транспонирования матрица");
+        for (int i = 0; i < m; i++) {
+            System.out.println();
+            for (int j = 0; j < n; j++) {
+                System.out.print(arrayTranspose[i][j]);
             }
         }
 
