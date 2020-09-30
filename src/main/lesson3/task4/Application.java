69a24b7ffda48aecc0f70723644bf703e9f9a341
mgrishchenkova <71334332+mgrishchenkova@users.noreply.github.com>
2020-09-30 21:03:14 +0300

    задача4
    Signed-off-by: mgrishchenkova <71334332+mgrishchenkova@users.noreply.github.com>

--- src/main/lesson3/task4/Application.java
+++ src/main/lesson3/task4/Application.java
@@ -1,4 +1,33 @@
 package main.lesson3.task4;
 
 public class Application {
+    public static void main(String[] args) {
+        Human sergey = new Human("Сергей");
+        Human valeriy = new Human("Валерий");
+        Human mikhail = new Human("Михаил");
+        Human stanislav = new Human("Станислав");
+        Human egor = new Human("Егор");
+        Human grigoriy = new Human("Григорий");
+        Human alexander = new Human("Александр");
+
+        sergey.addfriends(valeriy);
+        sergey.addfriends(grigoriy);
+        stanislav.addfriends(valeriy);
+        stanislav.addfriends(mikhail);
+        egor.addfriends(alexander);
+        egor.addfriends(grigoriy);
+        alexander.addfriends(grigoriy);
+        alexander.addfriends(mikhail);
+
+        System.out.println(proverka(sergey, grigoriy));
+        System.out.println(proverka(sergey, alexander));
+        System.out.println(proverka(stanislav, mikhail));
+        System.out.println(proverka(mikhail, valeriy));
+    }
+
+    static boolean proverka(Human first, Human second) {
+        for (Human firstFriend : first.friends)
+            if (firstFriend == second) return true;
+        return false;
+    }
 }
--- src/main/lesson3/task4/Human.java
+++ src/main/lesson3/task4/Human.java
@@ -1,4 +1,24 @@
 package main.lesson3.task4;
 
 public class Human {
+    String name;
+    Human[] friends = new Human[5];
+
+    Human (String name){
+        this.name=name;
+
+    }
+
+    void addfriends(Human adfriends){
+        int i=0;
+        while (friends[i] != null) {
+            if (friends[i] == adfriends) return;
+            i++;
+        }
+        friends[i] = adfriends;
+        adfriends.addfriends(this);
+    }
+
+
 }
+
