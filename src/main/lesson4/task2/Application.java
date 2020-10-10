a109ef45a6450ae28b1f196bd25e3825efc8e164
mgrishchenkova <71334332+mgrishchenkova@users.noreply.github.com>
2020-10-10 18:50:10 +0300

    d
    Signed-off-by: mgrishchenkova <71334332+mgrishchenkova@users.noreply.github.com>

--- src/main/lesson4/task2/Application.java
+++ src/main/lesson4/task2/Application.java
@@ -1,23 +1,36 @@
 package main.lesson4.task2;
 
+import java.io.IOException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException, IOException {
         SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+        Human[] human = new Human[2];
 
-        Human[] human=new Human[2];
-        while (true){
+        while (true) {
             Scanner s = new Scanner(System.in);
             System.out.println("Введите строку в формате ");
             String str = s.nextLine();
-            Validator.validStr(str);
-            Validator.razdelitel(str);
+            if (!Validator.validStr(str) == true) continue;
+            {
+                String[] strRaz = Validator.razdelitel(str);
+                Human human1 = new Human();
+                human1.setLastName(strRaz[0]);
+                human1.setFirstName(strRaz[1]);
+                human1.setBirthDate(sdf.parse(strRaz[2]));
+                System.out.println("все ок");
+              ;
+                if (! Validator.validHuman(human1)) continue;
+                int i = 0;
+                human[i++] = human1;
+
+                if (!Validator.validArrayHuman(human)) break;
+            }
         }
-
-
 
     }
 }
--- src/main/lesson4/task2/Validator.java
+++ src/main/lesson4/task2/Validator.java
@@ -3,7 +3,7 @@ package main.lesson4.task2;
 public class Validator {
 
     private String str;
-    private static  String regex = "^(\\S+) (\\S+) (\\S+)$";
+    private static String regex = "^(\\S+) (\\S+) (\\S+)$";
 
     public void setStr(String str) {
         this.str = str;
@@ -13,52 +13,52 @@ public class Validator {
         return str;
     }
 
-    public static void validStr(String str) {
-
+    public static boolean validStr(String str) {
+        boolean isValidStr;
         String regex = "^(\\S+) (\\S+) (\\S+)$";
         if (str.matches(regex)) {
-            System.out.println("Строка валидна");
+            return isValidStr = true;
 
-        } else {
-            System.out.println("Строка невалидна");
-        }
+        } else return isValidStr = false;
 
     }
 
-    public static void razdelitel (String str){
+    public static String[] razdelitel(String str) {
         String delimeter = " ";
         String[] subStr;
         subStr = str.split(delimeter);
         // Вывод результата на экран
-        for(int i = 0; i < subStr.length; i++) {
+        for (int i = 0; i < subStr.length; i++) {
             System.out.println(subStr[i]);
         }
+ return subStr;
+    }
+
+    public static boolean validHuman(Human human) {
+
+        if (human.getLastName() != null & human.getFirstName() != null & human.getBirthDate() != null) {
+            System.out.println("Массив валиден 1");
+            return true;
+
+        } else
+            System.out.println("Массив невалиден 1");
+        return false;
+
 
     }
- public static boolean validHuman (Human human) {
 
-     if (human.getLastName() != null & human.getFirstName() != null & human.getBirthDate() != null) {
-         System.out.println("Массив валиден");
-         return true;
+    public static boolean validArrayHuman(Human[] human) {
+        boolean valid = true;
+        for (Human array : human) {
+            if (array == null) {
+                System.out.println("Массив заполнен");
+                valid = false;
+                break;
+            } else System.out.println("Массив не заполнен");
 
-     } else
-         System.out.println("Массив невалиден");
-         return false;
-
-
- }
- public static boolean validArrayHuman ( Human[] human){
-        boolean valid=false;
-     for (Human array : human) {
-         if (array == null) {
-             System.out.println("Массив невалиден");
-             valid= true;
-             break;
-         } else System.out.println("Массив валиден");
-
-     }
-     return valid;
- }
+        }
+        return valid;
+    }
 
 
 }
