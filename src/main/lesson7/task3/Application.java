6d64b8b389d433b0cdba0e2e1b35f67e39854217
mgrishchenkova <71334332+mgrishchenkova@users.noreply.github.com>
2020-10-22 23:03:47 +0300

    d
    Signed-off-by: mgrishchenkova <71334332+mgrishchenkova@users.noreply.github.com>

--- src/main/lesson7/task3/Application.java
+++ src/main/lesson7/task3/Application.java
@@ -2,6 +2,7 @@ package main.lesson7.task3;
 
 import main.lesson7.task3.Figures.ChessFigure;
 import main.lesson7.task3.Figures.King;
+import main.lesson7.task3.Figures.Knight;
 import main.lesson7.task3.chess.Chess;
 import main.lesson7.task3.chess.Position;
 
@@ -10,8 +11,10 @@ import java.util.Set;
 public class Application {
     public static void main(String[] args) {
         Position position = new Position("A2");
-        ChessFigure king = new King(position);
-        Set<Position> kingPos = king.getAvailableMoves();
+     //   ChessFigure king = new King(position);
+        ChessFigure knight = new Knight(position);
+        Set<Position> knightPos = knight.getAvailableMoves();
+      //  Set<Position> kingPos = king.getAvailableMoves();
         System.out.println();
     }
 }
--- src/main/lesson7/task3/Figures/Knight.java
+++ src/main/lesson7/task3/Figures/Knight.java
@@ -1,4 +1,34 @@
 package main.lesson7.task3.Figures;
 
-public class Knight {
+import main.lesson7.task3.chess.Chess;
+import main.lesson7.task3.chess.Position;
+
+import java.io.PipedOutputStream;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Knight extends ChessFigure{
+
+    public Knight(Position position) {
+        super("Конь", position);
+    }
+
+    @Override
+    public Set<Position> getAvailableMoves() {
+        Set<Position> allChess = Chess.getChess();
+        Set<Position> addKnight = new HashSet<>();
+        for (Position candidate: allChess)
+            if(isMove(position, candidate))
+                addKnight.add(position);
+            return addKnight;
+
+    }
+
+    private boolean isMove(Position figurePosition, Position candidate){
+        int dx=Math.abs(figurePosition.getHorizontally()-candidate.getHorizontally());
+        int dy=Math.abs(figurePosition.getVertically()-candidate.getVertically());
+        if (dx==2 && dy==1) return true;
+        if (dx==1 && dy==2) return true;
+        return false;
+    }
 }
