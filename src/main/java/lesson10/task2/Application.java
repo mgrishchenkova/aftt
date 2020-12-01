6de5864253d683e90b6aa321c46b9574228e7104
mgrishchenkova <71334332+mgrishchenkova@users.noreply.github.com>
2020-12-01 19:54:38 +0300

    d
    Signed-off-by: mgrishchenkova <71334332+mgrishchenkova@users.noreply.github.com>

--- src/main/java/lesson10/task2/Application.java
+++ src/main/java/lesson10/task2/Application.java
@@ -6,13 +6,13 @@ import java.util.List;
 import static lesson10.task2.FileHelper.write;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         List<String> generatedContent = new ArrayList<>();
 
         for (int i = 0; i < Config.LINES_AMOUNT; i++) {
             generatedContent.add(StringHelper.generate());
         }
 
-        write(generatedContent);
+        FileHelper.write(generatedContent);
     }
 }
--- src/main/java/lesson10/task2/StringHelper.java
+++ src/main/java/lesson10/task2/StringHelper.java
@@ -1,6 +1,7 @@
 package lesson10.task2;
 
 import java.util.Properties;
+import java.util.Random;
 
 public class StringHelper {
     public static String generate() {
@@ -8,13 +9,14 @@ public class StringHelper {
         int LINES_MIN_LENGTH = Config.LINES_MIN_LENGTH;
         int LINES_MAX_LENGTH = Config.LINES_MAX_LENGTH;
         String LINES_PATTERN = Config.LINES_PATTERN;
+        int length = new Random().nextInt(LINES_MAX_LENGTH)+LINES_MIN_LENGTH;
         String LINES_MESSAGE = Config.LINES_MESSAGE;
 
-        for (int i = LINES_MIN_LENGTH; i <= LINES_MAX_LENGTH; i++) {
-            sb.append(LINES_PATTERN);
+        for (int i =0; i <=length; i++) {
+            sb.append(LINES_PATTERN.charAt(new Random().nextInt(LINES_PATTERN.length())));
         }
-        String result = sb.toString();
-        System.out.printf(LINES_MESSAGE, result);
+      String result =  sb.toString();
+       System.out.printf(LINES_MESSAGE, result);
         return result;
     }
 }
