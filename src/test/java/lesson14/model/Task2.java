d0478339a530d26106ef0141da67deaf501a4ff9
mgrishchenkova <71334332+mgrishchenkova@users.noreply.github.com>
2020-12-08 20:17:54 +0300

    d
    Signed-off-by: mgrishchenkova <71334332+mgrishchenkova@users.noreply.github.com>

---
+++ src/test/java/lesson14/model/Task2.java
@@ -0,0 +1,21 @@
+package lesson14.model;
+
+import lesson14.helpers.JsonHelper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class Task2 {
+    List<Person> persons = JsonHelper.getPersonsFromFile("src/test/java/lesson14/dataset.json");
+
+    @Test
+    void testTask2(){
+        long count=persons.stream()
+                .filter(person -> person.getLastName().equals("Васильев"))
+                .filter(person -> person.getCards().size()!=person.getAccounts().size())
+                .peek(person -> System.out.printf("%s %s %s%n", person.getLastName(), person.getFirstName(), person.getPatronymic()))
+                .count();
+        Assertions.assertEquals(10, count);
+    }
+}
