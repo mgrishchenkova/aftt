38f0791f871a3733dc2946c1b6f9fab1c0493d8b
mgrishchenkova <71334332+mgrishchenkova@users.noreply.github.com>
2020-12-11 22:15:06 +0300

    d
    Signed-off-by: mgrishchenkova <71334332+mgrishchenkova@users.noreply.github.com>

--- src/test/java/lesson14/Task12.java
+++ src/test/java/lesson14/Task12.java
@@ -9,24 +9,24 @@ import java.util.List;
 
 public class Task12 {
     List<Person> persons = PersonFilterHelper.getFilteredPersons();
+
     @Test
     void testOutPut() {
         persons.stream()
-               // .map(Person::getProperties)
-                .sorted((p1,p2)->{
+                // .map(Person::getProperties)
+                .sorted((p1, p2) -> {
                     Double summ1 = p1.getProperties().stream().map(Properties::getPrice).reduce(Double::sum).get();
                     Double summ2 = p2.getProperties().stream().map(Properties::getPrice).reduce(Double::sum).get();
                     return summ2.compareTo(summ1);
                 })
                 .forEach(person -> System.out.printf("ФИО: %s %s %s, Суммарная стоимость недвижимости: %.2f%n",
                         person.getLastName(), person.getFirstName(), person.getPatronymic(),
-                        person.getProperties().stream().map(Properties::getPrice).reduce((p1,p2)->p1+p2).get()
+                        person.getProperties().stream().map(Properties::getPrice).reduce((p1, p2) -> p1 + p2).get()
                 ));
 
 
     }
 
 
-
-    }
+}
 
---
+++ src/test/java/lesson14/Task13.java
@@ -0,0 +1,25 @@
+package lesson14;
+
+import lesson14.helpers.PersonFilterHelper;
+import lesson14.model.Person;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class Task13 {
+
+    List<Person> persons = PersonFilterHelper.getFilteredPersons();
+
+    @Test
+    void testOutputPass() {
+        persons.stream()
+                .sorted((p1, p2) -> {
+                    if (p1.getPassport().getSeries().equals(p2.getPassport().getSeries()))
+                        return p1.getPassport().getNumber().compareTo(p2.getPassport().getNumber());
+                    return p1.getPassport().getSeries().compareTo(p2.getPassport().getSeries());
+                })
+                .forEach(person -> System.out.printf("ФИО: %s %s %s, Паспорт: %s %s, Количество объектов недвижимости: %d%n»", person.getLastName(), person.getFirstName(), person.getPatronymic(),
+                        person.getPassport().getSeries(), person.getPassport().getNumber(), person.getProperties().size())
+                );
+    }
+}
